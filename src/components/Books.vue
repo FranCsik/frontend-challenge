8980f6b6cef7f70433e7f2865abf879446b6ceaf
Francisco Csik <francsik23@gmail.com>
2021-05-10 16:54:40 -0300

    challenge terminado

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -5,17 +5,87 @@
         <h4 class="title">NYTimes Books</h4>
       </div>
       <div class="search">
-        <input type="text" class="form-control" placeholder="Search by title" />
+        <select name="" id="">
+          <option value="" selected disabled >Choose a category</option>
+          <option v-bind:value="category.list_name_encoded" v-for="category in categories" :key='category.list_name_encoded'>
+            {{ category.display_name }}
+          </option>
+        </select>
+        <input type="text" class="form-control" placeholder="Search by title" v-on:keydown="searchBook" />
       </div>
       <div class="content">
-        <!-- ADD CONTENT HERE -->
+        <div v-for="book in books" :key="book.title" class="book" v-on:click="googleSearch">
+          <h1 class="bookTitle">{{ book.title }}</h1>
+          <h3 class="bookAuthor">{{ book.author }}</h3>
+          <p class="bookDescription">{{ book.description }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data(){
+    return {
+      books: false,
+      categories: [],
+      books: [],
+      api_key: 'aZJUSY3hvGpAAAKUEXLh9wUuNrbpxcNh'
+    };
+  },  
+  mounted(){
+    console.log('se montó el componente')
+    this.getBooks();
+  },
+  methods: {
+    getBooks(){
+      axios.get(`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${this.api_key}`) 
+        .then(response => {
+          for(let i = 0; i < 10; i++){
+            this.categories.push(response.data.results[i])
+          }
+        })       
+    },
+    searchBook: function(event){
+      let select = document.querySelector('select');
+      let input = document.querySelector('input');
+
+      if(event.key == 'Enter' && select.value == ''){
+        alert('You have to choose a category in order to search for a book')
+      }else if(event.key == 'Enter'){
+        let bookTitle = input.value.replace(' ', '+');
+
+        axios.get(`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?title=${bookTitle}&api-key=${this.api_key}`) 
+        .then(response => {
+          if(response.data.num_results != 0){
+            //si ya se hicieron busquedas, se borran de books
+            this.books = [];
+            for(let i = 0; i < response.data.results.length; i++){
+              this.books.push(response.data.results[i])
+            }
+          }else{
+            this.books = []
+            alert(`We coudn't find any book with with the following title:${input.value}.lease try again with another book title :)` )
+          }
+          console.log(this.books)
+        })
+        .catch(err => console.log(err)) 
+      }
+    },
+    googleSearch: function(event){
+      let title = event.currentTarget.querySelector('.bookTitle').innerHTML;
+      let author = event.currentTarget.querySelector('.bookAuthor').innerHTML;
+      console.log(title, author)
+      let search = title+' '+author;
+      console.log(search)
+      search = search.replace(/ /g, '+');
+      console.log(search)
+      location.href = `https://www.google.com/search?q=${search}`
+    }
+    
+  },
   name: "Books"
 };
 </script>
@@ -86,4 +156,41 @@ export default {
     width: 100%;
   }
 }
+.book {
+  margin-top:20px;
+  padding: 10px;
+  border: 1px solid black;
+}
+.bookTitle{
+  font-size: 25px;
+  font-weight: 700;
+}
+.bookAuthor{
+  font-size: 20px;
+  margin-top: 5px;
+}
+.bookDescription{
+  font-size: 16px;
+  margin-top: 3px;
+}
+
+@media(min-width: 768px){
+  .content{
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    margin: auto;
+  }
+  .book{
+    width: 300px;
+    height: 250px;
+    min-width: 300px;
+    cursor: pointer;
+  }
+}
+
+@media(min-width: 1024px){
+  
+}
+
 </style>
